ed869bb07bbdd8c782eb667fe9092aa1199ffc57
Анастасия Смирнова <pl_nastya@mail.ru>
2020-07-22 00:12:32 +0300

    made an api plan

---
+++ src/api/TaskService.ts
@@ -0,0 +1,21 @@
+export interface TaskServiceProps {
+  ajax: any;
+}
+
+export class TaskService {
+  props: TaskServiceProps;
+  constructor(props:TaskServiceProps) {
+    this.props = props;
+  }
+  // todo: получение списка категорий
+  // todo: получение списка задач (с указанным количеством и пагинацией)
+  // todo: получение ответов к задаче
+  // todo: создание задачи
+  // todo: ответ на задачу
+  // todo: изменение ответа к задаче
+  // todo: изменение задачи
+  // todo: удаление ответа
+  // todo: удаление задачи
+  // todo: получение всех задач по пользователю
+  // todo: получение всех ответов по пользователю
+}
---
+++ src/api/UserService.ts
@@ -0,0 +1,15 @@
+export interface UserServiceProps {
+  ajax: any;
+}
+
+export class UserService {
+  props: UserServiceProps;
+  constructor(props:UserServiceProps) {
+    this.props = props;
+  }
+  // todo: получение информации о пользователе
+  // todo: регистрация нового пользователя
+  // todo: логин
+  // todo: разлогин
+  // todo: сброс пароля
+}
